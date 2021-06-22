4006ff8a6a6ab4ae8de633f6aff691bb62fb8f8d
Gaurav Raj <techw803@gmail.com>
2021-06-22 15:29:05 +0530

    Updated the Deployed Script

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
+green="\e[1;32m"
+yellow="\e[1;33m"
+reset="\e[0m"
+
 if [[ $# == 1 ]]; then
-		echo "[*] Publishing the Site"
+		echo -e "[$yellow*$reset] Publishing the Site"
 		npm run deploy
-		echo "[+] Site Published Successfully..."
-		echo "[*] Backing Up the Configuration Files"
+		echo -e "[$green+$reset] Site Published Successfully..."
+		echo -e "[$yellow*$reset] Backing Up the Configuration Files"
 		git add . && git commit -m "$1" && git push -u origin code
-		echo "[+] Files Backup Completed..."
+		echo -e "[$green+$reset] Files Backup Completed..."
 else
-		echo "Usage: $0 'Git Commit Message'"
+		echo -e "Usage: $green$0$reset 'Git Commit Message'"
 fi
 
