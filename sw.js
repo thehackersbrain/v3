fa385cfe8aca8192f827ad290fc80af968ef104b
Gaurav Raj <techw803@gmail.com>
2021-12-15 20:08:48 +0530

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-887a8b663c19112bfcc1.js"
+    "url": "webpack-runtime-1d2c20f5561840a98012.js"
   },
   {
     "url": "framework-3321703c33570853116c.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "24c1231c3dd29cf61c2e5ec0ef273dff"
+    "revision": "b2ee7560a76c4d64201c266af4671a41"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-b1462017a2c34a95e2b4.js"
